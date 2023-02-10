3c03b21e744d9a154f6c0cf5e81c67639e321e37
Neil1977 <Neil_lunn1977@hotmail.com>
2023-02-10 16:26:03 +0000

    deleted Package.json and transferred KSB data js

---
+++ ksbData.js
@@ -0,0 +1,106 @@
+export const ksb = {
+    knowledge: [
+      {
+        title: 'K1',
+        text: 'all stages of the software development life-cycle (what each stage contains, including the inputs and outputs)',
+      },
+      {
+        title: 'K2',
+        text: 'roles and responsibilities within the software development lifecycle (who is responsible for what)',
+      },
+      {
+        title: 'K3',
+        text: 'the roles and responsibilities of the project life-cycle within your organisation, and your role',
+      },
+      {
+        title: 'K4',
+        text: 'how best to communicate using the different communication methods and how to adapt appropriately to different audiences',
+      },
+      {
+        title: 'K5',
+        text: 'the similarities and differences between different software development methodologies, such as agile and waterfall.',
+      },
+      { title: 'K6', text: 'how teams work effectively to produce software and how to contribute appropriately' },
+      {
+        title: 'K7',
+        text: 'software design approaches and patterns, to identify reusable solutions to commonly occurring problems',
+      },
+      {
+        title: 'K8',
+        text: 'organisational policies and procedures relating to the tasks being undertaken, and when to follow them. For example the storage and treatment of GDPR sensitive data.',
+      },
+      {
+        title: 'K9',
+        text: 'algorithms, logic and data structures relevant to software development for example:- arrays- stacks- queues- linked lists- trees- graphs- hash tables- sorting algorithms- searching algorithms- critical sections and race conditions',
+      },
+      { title: 'K10', text: 'principles and uses of relational and non-relational databases' },
+      { title: 'K11', text: 'software designs and functional or technical specifications' },
+      { title: 'K12', text: 'software testing frameworks and methodologies' },
+    ],
+    skills: [
+      { title: 'S1', text: 'create logical and maintainable code' },
+      { title: 'S2', text: 'develop effective user interfaces' },
+      { title: 'S3', text: 'link code to data sets' },
+      { title: 'S4', text: 'test code and analyse results to correct errors found using unit testing' },
+      {
+        title: 'S5',
+        text: 'conduct a range of test types, such as Integration, System, User Acceptance, Non-Functional, Performance and Security testing.',
+      },
+      { title: 'S6', text: 'identify and create test scenarios' },
+      {
+        title: 'S7',
+        text: 'apply structured techniques to problem solving, debug code and understand the structure of programmes in order to identify and resolve issues',
+      },
+      { title: 'S8', text: 'create simple software designs to effectively communicate understanding of the program' },
+      { title: 'S9', text: 'create analysis artefacts, such as use cases and/or user stories' },
+      { title: 'S10', text: 'build, manage and deploy code into the relevant environment' },
+      {
+        title: 'S11',
+        text: 'apply an appropriate software development approach according to the relevant paradigm (for example object oriented, event driven or procedural)',
+      },
+      { title: 'S12', text: 'follow software designs and functional or technical specifications' },
+      { title: 'S13', text: 'follow testing frameworks and methodologies' },
+      {
+        title: 'S14',
+        text: 'follow company, team or client approaches to continuous integration, version and source control',
+      },
+      { title: 'S15', text: 'communicate software solutions and ideas to technical and non-technical stakeholders' },
+      { title: 'S16', text: 'apply algorithms, logic and data structures' },
+      {
+        title: 'S17',
+        text: 'interpret and implement a given design whist remaining compliant with security and maintainability requirements',
+      },
+    ],
+    behaviours: [
+      {
+        title: 'B1',
+        text: 'Works independently and takes responsibility. For example, has a disciplined and responsible approach to risk and stays motivated and committed when facing challenges',
+      },
+      {
+        title: 'B2',
+        text: 'Applies logical thinking. For example, uses clear and valid reasoning when making decisions related to undertaking work instructions',
+      },
+      { title: 'B3', text: 'Maintains a productive, professional and secure working environment' },
+      {
+        title: 'B4',
+        text: 'Works collaboratively with a wide range of people in different roles, internally and externally, with a positive attitude to inclusion & diversity',
+      },
+      {
+        title: 'B5',
+        text: 'Acts with integrity with respect to ethical, legal and regulatory ensuring the protection of personal data, safety and security.',
+      },
+      {
+        title: 'B6',
+        text: 'Shows initiative and takes responsibility for solving problems within their own remit, being resourceful when faced with a problem to solve.',
+      },
+      {
+        title: 'B7',
+        text: 'Communicates effectively in a variety of situations to both a technical and non-technical audience.',
+      },
+      {
+        title: 'B8',
+        text: 'Shows curiosity to the business context in which the solution will be used, displaying an inquisitive approach to solving the problem. This includes the curiosity to explore new opportunities, techniques and the tenacity to improve methods and maximise performance of the solution and creativity in their approach to solutions.',
+      },
+      { title: 'B9', text: 'Committed to continued professional development.' },
+    ],
+  };
