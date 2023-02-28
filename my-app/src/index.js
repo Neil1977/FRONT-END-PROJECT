3561f1544cd0dbe81663b9e514a630f28d7a57b5
Neil1977 <Neil_lunn1977@hotmail.com>
2023-02-28 10:50:24 +0000

    updated branch

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,7 +1,20 @@
 import React, { Component } from "react";
 import { Card } from "Card.jsx";
 
-/*createRoot(document.getElementById('root')).render(<App />); - REPLACE BELOW?*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*createRoot(document.getElementById('root')).render(<App />); - REPLACE BELOW?
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
